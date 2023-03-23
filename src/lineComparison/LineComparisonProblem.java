1b28834e95f4d3bb51c2d439b4b910283594907e
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-23 23:02:07 +0530

    Add: Code for calculating length of line.

--- src/lineComparison/LineComparisonProblem.java
+++ src/lineComparison/LineComparisonProblem.java
@@ -1,5 +1,21 @@
 package lineComparison;
 
-public class LineComparisonProblem {
+import java.util.Scanner;
 
+public class LineComparisonProblem {
+	public static void main(String[] args) {
+		System.out.println("Calculate the length of a line using Cartecian co-ordinate system.");
+
+		System.out.println("Enter the value of first point :");
+		Scanner a = new Scanner(System.in);
+		int x1 = a.nextInt();
+		int y1 = a.nextInt();
+		
+		System.out.println("Enter the value of secound point :");
+		int x2 = a.nextInt();
+		int y2 = a.nextInt();
+		
+		int length = (int)(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+		System.out.println("Length of a line = "+Math.sqrt(length));
+	}
 }
